8c1084d0afac4bc262b7be65c4ed1459c9759796
rasuar42 <rasuar42@users.noreply.github.com>
2020-09-13 05:21:01 -0400

    Added encrypt/decrypt methods

--- suarez_p1/src/Decrypter.java
+++ suarez_p1/src/Decrypter.java
@@ -1,2 +1,29 @@
 public class Decrypter {
+
+    public static String decrypt(String input)
+    {
+        String output = "";
+        int temp;
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            temp = Character.getNumericValue(input.charAt(i));
+            temp += 3;
+            temp = temp%10;
+            output+=temp;
+        }
+        output = switchNumbers(output);
+
+        return output;
+    }
+
+    private static String switchNumbers(String str)
+    {
+        char s3 = str.charAt(0);
+        char s4 = str.charAt(1);
+        char s1 = str.charAt(2);
+        char s2 = str.charAt(3);
+
+        return ""+s1+s2+s3+s4;
+    }
 }
--- suarez_p1/src/Encrypter.java
+++ suarez_p1/src/Encrypter.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Encrypter {
+public class Encrypter {
+
+    public static String encrypt(String input)
+    {
+        String output = "";
+        int temp;
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            temp = Character.getNumericValue(input.charAt(i));
+            temp += 7;
+            temp = temp%10;
+            output+=temp;
+        }
+        output = switchNumbers(output);
+
+        return output;
+    }
+
+    private static String switchNumbers(String str)
+    {
+        char s1 = str.charAt(0);
+        char s2 = str.charAt(1);
+        char s3 = str.charAt(2);
+        char s4 = str.charAt(3);
+
+        return ""+s3+s4+s1+s2;
+    }
 }
