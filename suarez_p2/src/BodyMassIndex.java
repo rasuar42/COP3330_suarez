6b6f4a88329cc8c6881657a4a88b130a50438f1d
rasuar42 <rasuar42@users.noreply.github.com>
2020-09-28 16:47:37 -0400

    Reworked Class

--- suarez_p2/src/BodyMassIndex.java
+++ suarez_p2/src/BodyMassIndex.java
@@ -3,36 +3,32 @@ import  java.lang.Math;
 public class BodyMassIndex {
     private final double height;
     private final double weight;
-    private final double BMI;
-
 
     //Constructor
     public BodyMassIndex(double h, double w)
     {
         height = h;
         weight = w;
-        BMI = calcBMI(h, w);
     }
 
-    //Helper Functions
-
-    /* Unneeded Functions
-    public double getHeight() {
-        return height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-    */
-
-    public double getBMI() {
-        return BMI;
-    }
-
-    private static double calcBMI(double h, double w)
+    public String categorize()
     {
-        double temp = (703 * w) / (h * h);
+        double BMI = calcBMI();
+
+        if(BMI > 29.9) {
+            return "Obesity";
+        }else if(BMI > 24.9) {
+            return "Overweight";
+        }else if(BMI > 18.4) {
+            return "Normal Weight";
+        }else{
+            return "Underweight";
+        }
+    }
+
+    public double calcBMI()
+    {
+        double temp = (703 * weight) / (height * height);
         return (double)Math.round(temp * 10) / 10;  //rounded one number after decimal
     }
 }
